68993627d4e8a4667e5abf82af0922ed98781cf2
abdullox0900 <abdullox0900@gmail.com>
2022-01-22 00:58:20 +0500

    add js funtions

--- js/script.js
+++ js/script.js
@@ -5,8 +5,28 @@ document.body.style.backgroundColor = "#E0F0FD";
 let elForm = document.querySelector(".form");
 let elInput = document.querySelector(".form__input");
 let elList = document.querySelector(".list");
+let elBtnClear = document.querySelector(".btn-crear");
 
 let todosArr = [];
+
+elList.addEventListener("click", function (evt) {
+    if (evt.target.matches(".close__btn")) {
+        let todoIdBtn = evt.target.dataset.todoId * 1;
+        let todoFound = todosArr.findIndex((todo) => todo.id === todoIdBtn);
+
+        todosArr.splice(todoFound, 1);
+
+        elList.innerHTML = null;
+
+        todoRender(todosArr, elList)
+    }
+})
+
+elBtnClear.addEventListener("click", function () {
+    // elList.innerHTML = null;
+    todosArr.splice(0, todosArr.length);
+    elList.innerHTML = null;
+})
 
 let todoRender = function (arr, element) {
     arr.forEach(newArr => {
@@ -17,6 +37,7 @@ let todoRender = function (arr, element) {
         elCloseBtn.setAttribute("class", "close__btn");
 
         elListItem.textContent = newArr.name
+        elCloseBtn.dataset.todoId = newArr.id;
 
         element.appendChild(elListItem);
         elListItem.appendChild(elCloseBtn);
@@ -30,7 +51,7 @@ elForm.addEventListener("submit", function (evt) {
     let inputValue = elInput.value;
 
     let todosObj = {
-        id: todosArr.length,
+        id: todosArr.length * 1,
         name: inputValue,
         isCompleted: false,
     }
